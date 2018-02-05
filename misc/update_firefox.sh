51236b36da9ade76fca24b814865d57e686825df
lotib <boit.sanssoif@gmail.com>
2018-02-05 23:16:19 +0100

    get and install the last firefox version

---
+++ misc/update_firefox.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# as root
+#
+# get latest firefox on Debian
+#
+
+wget -O /tmp/FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
+tar xjf /tmp/FirefoxSetup.tar.bz2 -C /opt/firefox/
+mv /usr/lib/firefox-esr/firefox-esr /usr/lib/firefox-esr/firefox-esr.old
+ln -s /opt/firefox/firefox/firefox /usr/lib/firefox-esr/firefox-esr
