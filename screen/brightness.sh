4fdf0add4088a1ffee438d258d2d024db25b9599
lotib <boit.sanssoif@gmail.com>
2018-05-04 17:43:38 +0200

    add rate

--- screen/brightness.sh
+++ screen/brightness.sh
@@ -6,3 +6,9 @@ SCREEN=$(xrandr -q | grep " connected" | awk '{print $1}')
 # 0.1 => ?
 xrandr --output ${SCREEN} --brightness 1
 
+# specify rate
+# xrandr --output eDP-1 --mode 1920x1080 --rate 59.93
+
+
+
+
