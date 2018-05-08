367a7043f0961fa9c7ebd97f9af27d601c77774d
lotib <boit.sanssoif@gmail.com>
2018-05-08 17:39:07 +0200

    backlight value

--- screen/brightness.sh
+++ screen/brightness.sh
@@ -9,6 +9,8 @@ xrandr --output ${SCREEN} --brightness 1
 # specify rate
 # xrandr --output eDP-1 --mode 1920x1080 --rate 59.93
 
-
+# cat /sys/class/backlight/intel_backlight/max_brightness
+# cat /sys/class/backlight/intel_backlight/brightness
+# cat /sys/class/backlight/intel_backlight/actual_brightness
 
 
