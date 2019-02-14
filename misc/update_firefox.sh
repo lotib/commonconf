5bb8333782b318f1f1c36177859c141401bbada4
lotib <boit.sanssoif@gmail.com>
2019-02-14 04:09:19 +0100

    create dir if not exists

--- misc/update_firefox.sh
+++ misc/update_firefox.sh
@@ -5,6 +5,7 @@
 # get latest firefox on Debian
 #
 
+mkdir -p /opt/firefox
 wget -O /tmp/FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
 tar xjf /tmp/FirefoxSetup.tar.bz2 -C /opt/firefox/
 mv /usr/lib/firefox-esr/firefox-esr /usr/lib/firefox-esr/firefox-esr.old
